c7dce9132bc8ddfb08c1f3a946c28944dd7d254b
Darko Pranjic <darkopranjic.4@gmail.com>
2018-03-27 10:00:56 +0200

    Removed node_modules

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,7 +1,7 @@
-import bcrypt from 'bcrypt'
-import _ from 'lodash'
+import bcrypt from "bcrypt";
+import _ from "lodash";
 
-import { tryLogin } from '../auth'
+import { tryLogin } from "../auth";
 
 const formatErrors = (e, models) => {
   // If validation error from sequelize
@@ -9,38 +9,35 @@ const formatErrors = (e, models) => {
     /**
      * _.pick({a: 1, b: 2}, 'a') returns {a:1}
      */
-    return e.errors.map(x => _.pick(x, ["path", "message"]))
+    return e.errors.map(x => _.pick(x, ["path", "message"]));
   }
 
-  return [{ path: "name", message: "something went wrong" }]
-}
+  return [{ path: "name", message: "something went wrong" }];
+};
 
 export default {
   Query: {
-    getUser: (parent, args, { models }) =>
+    getUser: (parent, { id }, { models }) =>
       models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {
-    login: (parent, { email, password }, { models, SECRET, SECRET2 }) => tryLogin(email, password, models, SECRET, SECRET2),
+    login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
+      tryLogin(email, password, models, SECRET, SECRET2),
     register: async (parent, args, context) => {
-
       try {
-
-        const user = await context.models.User.create(args)
+        const user = await context.models.User.create(args);
 
         return {
           ok: true,
           user
         };
-
       } catch (error) {
         return {
           ok: false,
           errors: formatErrors(error, context.models)
         };
       }
-
     }
   }
 };
